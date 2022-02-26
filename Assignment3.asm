de0bf1fcab977f6bf46b5c74dad05e97f3a602bc
John Caruthers <john.d.caruthers@gmail.com>
2022-02-26 16:06:21 -0500

    correction to strg2
    change data type from .ascii to .asciz and correction to memory location of spc1

--- Assignment3.asm
+++ Assignment3.asm
@@ -14,8 +14,8 @@ num3:	.half	65535	# offset = 2, memory address = 0x10010002
 num4:	.half	255	# offset = 4, memory address = 0x10010004
 num5:	.word	-1	# offset = 8, memory address = 0x10010008
 strg1:	.ascii	"A1 B2 C3"	# offset = 12, memory address = 0x1001000c
-strg2:	.ascii	"Z"	# offset = 20, memory address = 0x10010014
-spc1:	.space	12	# offset = 21, memory address = 0x10010015
+strg2:	.asciz	"Z"	# offset = 20, memory address = 0x10010014
+spc1:	.space	12	# offset = 22, memory address = 0x10010016
 	
 	
 	.text
