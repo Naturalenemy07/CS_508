d861d442449ff88edeb76762021dc0845a6db199
John Caruthers <john.d.caruthers@gmail.com>
2022-02-28 07:32:45 -0500

    initial commit

---
+++ Assignment4.asm
@@ -0,0 +1,19 @@
+# Name: John Caruthers
+# Assignment: 4
+# Date: 28 Feb 22
+
+# Program Description: Arithmetic & Logic Operations, Register Management, Loading & Storing to Memory
+
+# Register Usage: 
+# 
+
+	.data
+
+	
+	
+	.text
+main:
+
+
+exit:	ori	a7, zero, 10	# program exit system call
+	ecall			# exit program
