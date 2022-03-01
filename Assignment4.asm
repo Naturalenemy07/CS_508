610c5bc5b538e2805d3d1a4296632cb80dabb86a
John Caruthers <john.d.caruthers@gmail.com>
2022-03-01 07:39:51 -0500

    continue adding hex values

--- Assignment4.asm
+++ Assignment4.asm
@@ -1,4 +1,5 @@
 # Name: John Caruthers
+# Course: CS 508: Computer Organization and Design
 # Assignment: 4
 # Date: 28 Feb 22
 
@@ -8,10 +9,10 @@
 # 
 
 	.data
-y_num:	.half	78	# offset: 0
-w_num:	.half	-540	# offset: 2
-x_num:	.byte	2	# offset: 4
-z_num:	.byte	-7	# offset: 5
+y_num:	.half	78	# offset: 0, HexValue: 0x004e
+w_num:	.half	-540	# offset: 2, Hexvalue: 0xfde4
+x_num:	.byte	3	# offset: 4, HexValue: 0x03
+z_num:	.byte	-7	# offset: 5, Hexvalue:
 c_num:	.half	121	# offset: 6
 d_num:	.byte	-10	# offset: 8 
 f_num:	.half	90	# offset: 9
