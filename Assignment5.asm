f71d05b90988cf137b45e99f48e5bc287d86eacc
John Caruthers <john.d.caruthers@gmail.com>
2022-03-27 07:56:39 -0400

    initial commit

---
+++ Assignment5.asm
@@ -0,0 +1,14 @@
+# Name: John Caruthers
+# Course: CS 508: Computer Organization and Design
+# Assignment: 5
+# Date: 27 Mar 22
+
+# Program Description: Branching Logic, Loops, Arrays
+
+# Register Usage: 
+
+
+	.data
+	
+	.text
+main:	lui	s0,0x10010	# Base Memory Address
