97e668a76a84fa57ec2eec0e9ef14fc2398d4a6f
John Caruthers <john.d.caruthers@gmail.com>
2022-04-11 06:49:38 -0400

    feedback
    inadverdently used pseudocode in previous versions, corrected this byt reversing the blt insturction inputs. Also, the branches in previous versions were outside of the loop.  I put them in the loop, but now the output is incorrect.  Still working on fixing the loop.

--- Assignment5.asm
+++ Assignment5.asm
@@ -1,7 +1,7 @@
 # Name: John Caruthers
 # Course: CS 508: Computer Organization and Design
 # Assignment: 5
-# Date: 27 Mar 22
+# Date: 11 Apr 22
 
 # Program Description: Branching Logic, Loops, Arrays
 
@@ -13,7 +13,7 @@
 # s4: offset
 # s5: sum of offset and address
 # s6: store number of values in array, this is hard coded
-# t0: temp display of values from array for me to test loop
+# t0: temporay storage of number prior to branching
 
 	.data
 arr:	.word	-160 -1830 922 -596 1432 -142 537 -94 2018 29	# offset: 0
@@ -28,11 +28,19 @@ main:	lui	s0,0x10010		# Base Memory Address
 	or	s3,zero,zero 	# initialize index to zero
 	addi	s6,zero,10		# store number of values in array (10) into s6
 	
+# Loop
 iterator:	slli	s4,s3,2		# multiply index by 4 to get offset
 	add	s5,s0,s4		# add offset to address and store as new address
 	lw	t0,0(s5)		# store from memory into t0 for further comparison	
-	bgt	t0,s1,branch_G	# go to greater branch if array[index] is greater than number stored in large number register
+	blt	s1,t0,branch_G	# go to greater branch if array[index] is greater than number stored in large number register
 	blt	t0,s2,branch_L	# go to lesser branch if array[index] is less than number stored in small number register
+	
+	# Branches
+branch_G:	add	s1,t0,zero		# store array[index] into large number register
+	beq	zero,zero,indaddex	# return to iterator for completion of loop
+	
+branch_L:	add	s2,t0,zero		# store array[index] into small number register
+	beq	zero,zero,indaddex	# return to iterator for completion of loop
 	
 indaddex:	addi	s3,s3,1		# add 1 to index
 	blt	s3,s6,iterator	# go back to beginning of loop if index is less than 10
@@ -41,11 +49,4 @@ indaddex:	addi	s3,s3,1		# add 1 to index
 	
 	ori	a7,zero,10		# Exit program
 	ecall
-
-# Branches
-branch_G:	add	s1,t0,zero		# store array[index] into large number register
-	beq	zero,zero,indaddex	# return to iterator for completion of loop
-	
-branch_L:	add	s2,t0,zero		# store array[index] into small number register
-	beq	zero,zero,indaddex	# return to iterator for completion of loop
 	
